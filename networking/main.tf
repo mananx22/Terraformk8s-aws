3274aa9c8053065443bbcce28e213452c80f2155
Ubuntu <ubuntu@ip-172-31-1-162.ap-south-1.compute.internal>
2021-08-26 07:22:00 +0000

    4.12 Addming Route Tables & Internet gAteway

--- networking/main.tf
+++ networking/main.tf
@@ -49,3 +49,42 @@ resource "aws_subnet" "mtc_private_subnet" {
     Name = "mtc_private_subnet-${count.index + 1}"
   }
 }
+
+
+resource "aws_route_table_association" "mtc_public_assoc" {
+  count = var.public_sn_count
+  subnet_id      = aws_subnet.mtc_public_subnet.*.id[count.index]
+  route_table_id = aws_route_table.mtc_public_rt.id
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "Manan_Gateway"
+  }
+}
+
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "Manan_public"
+  }
+}
+
+
+resource "aws_default_route_table" "mtc_private_rt" {
+  default_route_table_id = aws_vpc.mtc_vpc.default_route_table_id
+
+  tags = {
+    Name = "Manan_private"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id            = aws_route_table.mtc_public_rt.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
