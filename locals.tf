d6441daa5cc37cb7df52ba71226f144dc89daeec
Ubuntu <ubuntu@ip-172-31-1-162.ap-south-1.compute.internal>
2021-08-26 11:58:57 +0000

    4.16 adding dynamic sg for pub_pri sg

--- locals.tf
+++ locals.tf
@@ -1,11 +1,12 @@
 # ----- ROOT locals.tf
 
 locals {
-  vpc_cidr = "10.124.0.0/16"
+  vpc_cidr = "10.123.0.0/16"
 }
 
 locals {
   security_groups = {
+
     public = {
       name = "manan_sg"
       ingress = {
@@ -15,7 +16,29 @@ locals {
           protocol    = "tcp"
           cidr_blocks = [var.access_ip]
         }
+
+        http = {
+          from        = 80
+          to          = 80
+          protocol    = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
       }
     }
+
+
+    rds = {
+      name = "rds_sg"
+      ingress = {
+        mysql = {
+          from        = 3306
+          to          = 3306
+          protocol    = "tcp"
+          cidr_blocks = [local.vpc_cidr]
+        }
+      }
+    }
+
+
   }
 }
